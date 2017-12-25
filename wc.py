cfdd714507d84cfb949fb35319561821c6a789d6
Ramon Voges <ramon.voges@posteo.de>
2017-12-25 22:47:39 +0100

    Refactorin text and word_list

--- wc.py
+++ wc.py
@@ -4,9 +4,18 @@ class Wordcount(object):
     """Blueprint for the object Word_Count"""
 
     def __init__(self):
-        #  self.text = None
         self.dictionary = {}
-        #  self.count()
+
+    def pre_process_text(self, text):
+        """Replaces line endings and splits the text into an array.
+
+        :text: the given text input
+        :returns: text as an array to process
+
+        """
+        text = text.replace('\n', ' ')
+        word_list = text.split(' ')
+        return word_list
 
     def count(self, text):
         """Counts words in a given text.
@@ -15,10 +24,10 @@ class Wordcount(object):
 
         """
         self.dictionary = {}
+        word_list = self.pre_process_text(text)
         #  self.text = text
         #  text = text.split(' ')
-        text = text.replace('\n', ' ')
-        for word in text.split(' '):
+        for word in word_list:
             word = word.casefold()
             word = word.strip(',.!;?#')
             if self.dictionary.get(word):
