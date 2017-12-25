1fba606963d446f203355618dc9fbef0c65c0bc6
Ramon Voges <ramon.voges@posteo.de>
2017-12-25 22:29:55 +0100

    Interface changed
    - needs a method count(text)
    - tests adjusted

--- test_wc.py
+++ test_wc.py
@@ -6,20 +6,20 @@ def test_count_words():
     """Tests the basic functionality of Word_Count."""
 
     test = "Eins zwei eins drei"
-    counted = wc.Word_Count(test)
-    #  counted = counter.count(test)
-    assert counted.dictionary == {"eins": 2, "zwei": 1, "drei": 1}
+    counter = wc.Wordcount()
+    counted = counter.count(test)
+    assert counted == {"eins": 2, "zwei": 1, "drei": 1}
 
 def test_multiple_lines():
     """Tests the processing of multiple lines."""
 
     test = "Eins zwei\ndrei eins"
-    counted = wc.Word_Count(test)
-    assert counted.dictionary == {"eins": 2, "zwei": 1, "drei": 1}
+    counter = wc.Wordcount()
+    assert counter.count(test) == {"eins": 2, "zwei": 1, "drei": 1}
 
 def test_replace_punctuation():
     """Tests the removing of punctuation."""
 
     test = "Eins, zwei, drei, eins!"
-    counted = wc.Word_Count(test)
-    assert counted.dictionary == {"eins": 2, "zwei": 1, "drei": 1}
+    counter = wc.Wordcount()
+    assert counter.count(test) == {"eins": 2, "zwei": 1, "drei": 1}
--- wc.py
+++ wc.py
@@ -1,22 +1,22 @@
-import re
+#  import re
 
-class Word_Count(object):
+class Wordcount(object):
     """Blueprint for the object Word_Count"""
 
-    def __init__(self, text):
-        self.text = text
+    def __init__(self):
+        #  self.text = None
         self.dictionary = {}
-        self.count()
+        #  self.count()
 
-    def count(self):
+    def count(self, text):
         """Counts words in a given text.
 
         :returns: dictionary with words and counts
 
         """
+        #  self.text = text
         #  text = text.split(' ')
-        text = self.text.replace('\n', ' ')
-        #  text = text.replace(r'  +', ' ')
+        text = text.replace('\n', ' ')
         for word in text.split(' '):
             word = word.casefold()
             word = word.strip(',.!;?#')
@@ -30,5 +30,5 @@ class Word_Count(object):
         return self.dictionary
 
 if __name__ == "__main__":
-    counter = Word_Count("Eins zwei eins drei")
-    print(counter.dictionary)
+    wc = Wordcount()
+    print(wc.count("Eins zwei eins drei"))
